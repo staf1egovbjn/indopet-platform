24f3e6bdcb8b82b3ae128e8b40149249d545e4f1
Ahmad Fajrul Haq <110815597+staf1egovbjn@users.noreply.github.com>
2025-08-25 09:01:39 +0700

    Refactor middleware registration in app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,11 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+        
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
-        ]);
-        $middleware->use([
-            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
